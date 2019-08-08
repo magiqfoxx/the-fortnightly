4dd50abef1f7692da126b0238d726ca997965702
magiqfoxx <katarzyna.zemsta@vp.pl>
2019-08-08 21:16:54 +0200

    Updates

---
+++ precache-manifest.6dfd67411f7b4b1b055bcf642f20e638.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5421b53aa9c32570accd1d5caf87bd43",
+    "url": "/.github.io/the-fortnightly/index.html"
+  },
+  {
+    "revision": "36d50dc8bd20292e7e41",
+    "url": "/.github.io/the-fortnightly/static/js/2.4312bd85.chunk.js"
+  },
+  {
+    "revision": "9040240237600365ed5a",
+    "url": "/.github.io/the-fortnightly/static/js/main.f2a60983.chunk.js"
+  },
+  {
+    "revision": "54e81a80bbcc5342bc34",
+    "url": "/.github.io/the-fortnightly/static/js/runtime~main.c874694d.js"
+  }
+]);
